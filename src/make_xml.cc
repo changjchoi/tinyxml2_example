d3df85e9390b04edf849d638c637ba3001205290
warpizen <mmchang76@gmail.com>
2017-01-23 14:21:51 +0900

    static library issue

--- src/make_xml.cc
+++ src/make_xml.cc
@@ -2,36 +2,37 @@
 #include <vector>
 
 int main() {
-  tinyxml2::XMLDocument xmlDoc;
-  tinyxml2::XMLNode* pRoot = xmlDoc.NewElement("Root");
-  xmlDoc.InsertFirstChild(pRoot);
-
-  tinyxml2::XMLElement* pElement = xmlDoc.NewElement("IntValue");
-  pElement->SetText(10);
-  pRoot->InsertEndChild(pElement);
-
-  pElement = xmlDoc.NewElement("FloatValue");
-  pElement->SetText(0.5f);
-  pRoot->InsertEndChild(pElement);
-
-  pElement = xmlDoc.NewElement("Date");
-  pElement->SetAttribute("day", 26);
-  pElement->SetAttribute("month", "April");
-  pElement->SetAttribute("year", 2014);
-  pElement->SetAttribute("dateFormat", "26/04/2014");
-  pRoot->InsertEndChild(pElement);
-
+  tinyxml2::XMLDocument xmldoc;
+  // Insert first child
+  tinyxml2::XMLNode* root = xmldoc.NewElement("Root");
+  xmldoc.InsertFirstChild(root);
+  // <IntValue>10</IntValue>
+  tinyxml2::XMLElement* element = xmldoc.NewElement("IntValue");
+  element->SetText(10);
+  root->InsertEndChild(element);
+  // <FloatValue>0.5</FloatValue>
+  element = xmldoc.NewElement("FloatValue");
+  element->SetText(0.5f);
+  root->InsertEndChild(element);
+  // <Date day="23" month="January" year="2017" dateFormat="23/01/2017">
+  element = xmldoc.NewElement("Date");
+  element->SetAttribute("day", 23);
+  element->SetAttribute("month", "January");
+  element->SetAttribute("year", 2017);
+  element->SetAttribute("dateFormat", "23/01/2017");
+  root->InsertEndChild(element);
+  // Vector
   std::vector<int> vecList = {1,2,3,4,5,6,7,8,9};
-
-  pElement = xmlDoc.NewElement("List");
+  element = xmldoc.NewElement("List");
   for (const auto& item : vecList) {
-    tinyxml2::XMLElement* pListElement = xmlDoc.NewElement("Item");
+    tinyxml2::XMLElement* pListElement = xmldoc.NewElement("Item");
     pListElement->SetText(item);
-    pElement->InsertEndChild(pListElement);
+    element->InsertEndChild(pListElement);
   }
-  pElement->SetAttribute("itemCount", static_cast<int>(vecList.size()));
-  pRoot->InsertEndChild(pElement);
-  tinyxml2::XMLError eResult = xmlDoc.SaveFile("SavedData.xml");
+  element->SetAttribute("itemCount", static_cast<int>(vecList.size()));
+  root->InsertEndChild(element);
+  // Save to File
+  tinyxml2::XMLError eResult = xmldoc.SaveFile("SavedData.xml");
   //XMLCheckResult(eResult);
   return 0;
 }
